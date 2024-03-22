e9652f4cf2d4cde8e54930478e502052da9ac374
sagar01101100 <sagar01101100@gmail.com>
2024-03-23 02:34:13 +0530

    Resume changed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,11 +9,11 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  // const [width, setWidth] = useState(1200);
 
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+  // useEffect(() => {
+  //   setWidth(window.innerWidth);
+  // }, []);
 
   return (
     <div>
@@ -27,7 +27,7 @@ function ResumeNew() {
             style={{ maxWidth: "250px" }}
           >
             <AiOutlineDownload />
-            &nbsp;Download CV
+            &nbsp;Request For Resume
           </Button>
         </Row>
 
@@ -37,7 +37,7 @@ function ResumeNew() {
           </Document>
         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -47,10 +47,74 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row>
+        </Row> */}
       </Container>
     </div>
   );
 }
 
 export default ResumeNew;
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { Container, Row } from "react-bootstrap";
+// import Button from "react-bootstrap/Button";
+// import Particle from "../Particle";
+// import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+// import { AiOutlineDownload } from "react-icons/ai";
+// import { Document, Page, pdfjs } from "react-pdf";
+// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+// function ResumeNew() {
+//   const [width, setWidth] = useState(1200);
+
+//   useEffect(() => {
+//     setWidth(window.innerWidth);
+//   }, []);
+
+//   return (
+//     <div>
+//       <Container fluid className="resume-section">
+//         <Particle />
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Request For Resume
+//           </Button>
+//         </Row>
+
+//         <Row className="resume">
+//           <Document file={pdf} className="d-flex justify-content-center">
+//             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+//           </Document>
+//         </Row>
+
+//         <Row style={{ justifyContent: "center", position: "relative" }}>
+//           <Button
+//             variant="primary"
+//             href={pdf}
+//             target="_blank"
+//             style={{ maxWidth: "250px" }}
+//           >
+//             <AiOutlineDownload />
+//             &nbsp;Download CV
+//           </Button>
+//         </Row>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default ResumeNew;
